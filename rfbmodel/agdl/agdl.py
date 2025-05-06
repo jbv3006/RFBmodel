84e50dd9f213d45aa18774de4083b88a9d3e8d6a
felipehuerta17 <fnhuerta@ing.puc.cl>
2025-05-06 16:02:27 -0400

    various fixes to initialise class

---
+++ rfbmodel/agdl/agdl.py
@@ -0,0 +1,6 @@
+class Agdl:
+    def __init__(self, length, electronic_conductivity):
+
+        self.length=length
+        self.conductivity=electronic_conductivity    
+    
---
+++ rfbmodel/cathodes/__init__.py
@@ -0,0 +1,1 @@
+from .cathode import Cathode 

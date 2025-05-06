74e2002102e699dc30de74802c911cb8c2b06a67
felipehuerta17 <fnhuerta@ing.puc.cl>
2025-05-06 16:02:19 -0400

    setup to perform pip install .

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rfbmodel',
+    version='0.1',
+    packages=find_packages(),
+    description='Simulation suite for the evaporation of cryogenic liquids in storage tanks',
+    author='Joaquín Barros',
+    author_email= 'jbarrosvan@uc.cl',
+)

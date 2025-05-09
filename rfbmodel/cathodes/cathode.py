4b6e579f3b694782e0e2282faa1678844786b3a3
jbv3006 <jbarrosvan@uc.cl>
2025-05-09 18:44:00 -0400

    cathode mass balance

--- rfbmodel/cathodes/cathode.py
+++ rfbmodel/cathodes/cathode.py
@@ -11,19 +11,25 @@ class Cathode:
 
     def conc(self,SOC):
         # Define symbolic variables
-        c1, c2, c3, c4, c5, c6, c7, c8 = symbols('c1 c2 c3 c4 c5 c6 c7 c8')
-        #[VO_2^+, H+, VO^+2, H20, H2, H2SO4, HSO4-, SO4-2, SOC]
+        c1, c2, c3, c4, c5, c6  = vars
+        #[ VO^+2, VO_2^+, H+, HSO4-, SO4-2, H2SO4]
+        c_sulfate= 6.03  # Total sulfate concentration. 6 M or mol/L
+        c_vanadium= 1.03 #Total vanadium concentration. M or mol/L
+        Q_1= 199.5 #First dissociation quotient of H2SO4 [Adimensional]. Dependance on temperature is negligible
+        Q_2= 2.4 #Second dissociation quotient of H2SO4 [Adimensional]. Dependence on temperature is NOT negligble
 
         # equations
-        eq1 = Eq(c1 + c2 + 2*c3 - c7 - 2*c8, 0)               # electroneutrality
-        eq2 = Eq(SOC - c1 / (c1 + c3), 0)                    # SOC definition
-        eq3 = Eq(c2 * c1 - self.catholyte_props.K1 * c4, 0)  # First dissociation
-        eq4 = Eq(c3 * c1 - self.catholyte_props.K2 * c2, 0)  # Second dissociation
-        eq5 = Eq()               # 
-        eq6 = Eq()                    # 
-        eq7 = Eq()  # 
-        eq8 = Eq()  # 
-        eq9 = Eq()  # 
-        eqs = [eq1, eq2, eq3, eq4, eq5, eq6, eq7, eq8, eq9]
-        solution = solve(equations, (c1, c2, c3, c4, c5, c6, c7, c8), dict=True)
+        eq1 = 2*c1+c2+c3-c4-2*c5               # Electroneutrality
+        eq2 = SOC-c2/(c1+c2)                   # SOC definition
+        eq3 = c_sulfate - c4+c5+c6             # Total sulfate concentration
+        eq4 = c_vanadium - c1 - c2             # Total vanadium concentration
+        eq5 = Q_1*gamma_1- c3*c4/c6  # First dissociation. Falta término gamma de paper de Minnan
+        eq6 =  Q_2*gamma_2- c3*c5/c4 # Second dissociation. Falta término de los gamma
+
+        eqs = [eq1, eq2, eq3, eq4, eq5, eq6]
+
+        # Initial guesses
+        initial_guess = [0.5, 0.53, 3, 2.95, 0.08]
+        # Solve
+        solution = fsolve(equations, initial_guess)
         return solution

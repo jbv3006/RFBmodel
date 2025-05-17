094096a2cbd445cd22589b12961620e55b8f703f
jbv3006 <jbarrosvan@uc.cl>
2025-05-17 15:21:38 -0400

    Jupyter for cathode ready

--- build/lib/rfbmodel/cathodes/cathode.py
+++ build/lib/rfbmodel/cathodes/cathode.py
@@ -1,6 +1,6 @@
 from scipy.optimize import fsolve
 import numpy as np
-
+from rfbmodel.data import conductivity_funcV5, conductivity_funcV4
 
 class Cathode:
     def __init__(self):
@@ -17,11 +17,12 @@ class Cathode:
         self.Q_2 = 2.4          # Second dissociation quotient of H2SO4 [Adimensional]. Dependence on temperature is NOT negligible
         self.gamma_1 = 1
         self.gamma_2 = 1
-        self.thickness=50/1000 #Cathode thickness [m]
-        self.conductivity= 0 #Conductivity of the catholyte / sigma [S m^-1]
-        self.resistance=0 #Resistance of the catholyte / m^2/S
-        self.T=298 #Temperature of the cell [K]
-        print("holaa")
+        self.thickness = 50/1000 #Cathode thickness [m]
+        self.io_conductivity = 0 #Conductivity of the catholyte / sigma [S m^-1]
+        self.cath_conductivity = 4400 #Conductivity of the cathode / sigma [S m^-1]
+        self.resistance = 0 #Resistance of the catholyte / 
+        self.T = 298 #Temperature of the cell [K]
+        self.porosity = 0.94 #Cathode porosity (void fraction)
         pass
 
     def conc_sys(self, x):
@@ -40,7 +41,6 @@ class Cathode:
         eq4 = self.c_vanadium - c1 - c2             # Total vanadium concentration
         eq5 = self.Q_1 * self.gamma_1 - c3* (c4/c6)  # First dissociation. Falta término gamma de paper de Minnan
         eq6 =  self.Q_2 * self.gamma_2 - c3* (c5/c4) # Second dissociation. Falta término de los gamma
-        print("hola hola lele")
         
         # Construct the system as an array
         return np.array([eq1, eq2, eq3, eq4, eq5, eq6])
@@ -49,13 +49,22 @@ class Cathode:
         ''' Solves cathode concentration'''
         c = fsolve(self.conc_sys, x0 = self.c)
         # Set the equilibrium concentration in the cathode
-        print("lol")
         self.c = c
         
-
-    def cath_resistance(self):
-        ''' Solves cathode resistance'''
+    def ionic_resistance(self):
+        ''' Solves catholythe resistance considering void fraction'''
         # Set the conducttivity in the catholyte
-        print("loool")
-        self.conductivity= self.SOC*conductivity_funcV5(self.T)+(1-self.SOC)*conductivity_funcV4(self.T)
-        self.resistance=self.thickness/self.resistance
+        self.io_conductivity= self.SOC*conductivity_funcV5(self.T)+(1-self.SOC)*conductivity_funcV4(self.T)
+        # Set the resistance in the catholyte
+        self.io_resistance=self.thickness/(self.io_conductivity*self.porosity**1.5)
+
+    def cathode_resistance(self):
+        ''' Solves cathode electronic conductivity considering void fraction'''
+        # Set the resistance in the cathode
+        print("cathodo")
+        self.cath_resistance=self.thickness/(self.cath_conductivity*(1-self.porosity)**1.5)
+
+    def total_resistance(self):
+        ''' Solves electronic and ionic resistance of the cathode and catholyte'''
+        # Set the total resistance in the cathode and catholyte
+        self.resistance=(1/self.cath_resistance+1/self.io_resistance)**(-1)
